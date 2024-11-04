b43c11212a8cecec05a4454aa700170342cd8cbe
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-11-04 23:43:24 +0100

    Added functions file
    - Functions.py is used to organisate main functionsalitys to be imported in the main programm
    - moved the player greeting to this function

---
+++ functions.py
@@ -0,0 +1,16 @@
+def greeting() -> None:
+    print("""
+   ____         __  __             __  __  __            __
+  / __/______  / /_/ /__ ____  ___/ /  \ \/ /__ ________/ /
+ _\ \/ __/ _ \/ __/ / _ `/ _ \/ _  /    \  / _ `/ __/ _  / 
+/___/\__/\___/\__/_/\_,_/_//_/\_,_/     /_/\_,_/_/  \_,_/  
+                                                           
+""")
+   
+    print("Let's play a game!")
+    print("You are a detective in London.")
+    print("The goal is to find the criminal.")
+    print("You will be able to find tickets to use the underground and even the boat,")
+    print("but be careful! The tickets are only valid two times.")
+    print("Have fun!")
+
--- project_adventure_game_introduction_to_python1.5.py
+++ project_adventure_game_introduction_to_python1.5.py
@@ -19,6 +19,7 @@
 
 from random import randint
 from places import Desc, Objects, North, South, East, West, Up, Down
+from functions import greeting
 
 # setting beginning variables of payer and computer
 
@@ -45,23 +46,7 @@ inhand = " "
 transition = " "
 moves = 0
 
-
-
-print("""
-   ____         __  __             __  __  __            __
-  / __/______  / /_/ /__ ____  ___/ /  \ \/ /__ ________/ /
- _\ \/ __/ _ \/ __/ / _ `/ _ \/ _  /    \  / _ `/ __/ _  / 
-/___/\__/\___/\__/_/\_,_/_//_/\_,_/     /_/\_,_/_/  \_,_/  
-                                                           
-""")
-   
-print("Let's play a game!")
-print("You are a detective in London.")
-print("The goal is to find the criminal.")
-print("You will be able to find tickets to use the underground and even the boat,")
-print("but be careful! The tickets are only valid two times.")
-print("Have fun!")
-
+greeting()
 
 while not quitting:
     while command != "N" and command != "S" and command != "W" and command != "E" and command != "U" and command != "D" and command != "X" and command != "P" and command != "K" and command != "Q" :     
